3c8fb9c5a9b128046bb4f637494e24119d700539
Test <test@Tests-MacBook.local>
2023-08-06 14:49:01 +0200

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,26 @@
+class NumArray {
+
+    int[] answer;
+
+    public NumArray(int[] nums) {
+        answer = new int[nums.length];
+        for(int i = 0; i < nums.length; i++){
+            answer[i] = nums[i];
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        int sum = 0;
+
+        for(int i = left; i <= right; i++){
+            sum += answer[i];
+        }
+        return sum;
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
